bf25572a9f61f1f9b5a12d184ed409ee54ae454d
Jodus-Melodus <philipgricius@gmail.com>
2025-06-03 15:44:42 +0200

    update version

---
+++ target/debug/deps/vector_2d_3d-f29eb9bcae1a842f.d
@@ -0,0 +1,5 @@
+C:\Users\Philip\Documents\Documents\programming\libraries\rust\vector\target\debug\deps\libvector_2d_3d-f29eb9bcae1a842f.rmeta: src\lib.rs
+
+C:\Users\Philip\Documents\Documents\programming\libraries\rust\vector\target\debug\deps\vector_2d_3d-f29eb9bcae1a842f.d: src\lib.rs
+
+src\lib.rs:
